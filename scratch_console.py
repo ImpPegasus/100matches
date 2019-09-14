6700aa1768c2748f1b643a42c62c59a2c8ae43df
safronov <tim.safronov11@gmail.com>
2019-09-14 12:04:36 +0700

    Added function for AI turn

--- scratch_console.py
+++ scratch_console.py
@@ -1,7 +1,23 @@
-import random
+from random import randrange
 
+play = True
+vMax = 100
+
+
+def AIturn():
+    nonlocal Num, Count
+    if Num > Count:
+        Num = Count;
+    else:
+        Num = randrange(0, 10)
 
 def __main__():
+    Num = 0
+    Count = vMax
+    AIturn()
+    while play:
+        print('Number of opponents')
+
     return 0
 
 
