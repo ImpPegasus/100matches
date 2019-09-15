7be3bfb2a41d7f5118510615c684654933aa3075
timofey1234 <tim.safronov11@gmail.com>
2019-09-15 18:30:32 +0700

    Added game function to console

--- scratch_console.py
+++ scratch_console.py
@@ -1,7 +1,5 @@
-from random import randrange
-
 play = True
-vMax = 100
+MAX = 100
 
 
 def AIturn():
@@ -10,13 +8,70 @@ def AIturn():
         Num = Count;
     else:
         Num = randrange(0, 10)
+    return 0
 
+
+def start(NumOfPlayers: int):
+    nonlocal names
+    print("Please, enter names of all players")
+    for name in range(0, NumOfPlayers):
+        input(names[name])
+    for name in range(4, 1 + NumOfPlayers):
+        names[name] = 'AI ' + str(name)
+    return 0
+
+
+def game(plr: int, NumOfPlayers: int, op: int):
+    nonlocal names, Num, Count
+    if plr == 1:
+        print("It's ", names[1], " turn!")
+        print('Table has ', Count, ' matches, how much would you take?')
+        input(Num)
+        if type(Num) != int:
+            print('Incorrect value, please try again')
+    elif plr == 2:
+        print("It's ", names[2], " turn!")
+        print('Table has ', Count, ' matches, how much would you take?')
+        input(Num)
+        if type(Num) != int:
+            print('Incorrect value, please try again')
+
+    elif plr == 3:
+        print("It's ", names[3], " turn!")
+        print('Table has ', Count, ' matches, how much would you take?')
+        input(Num)
+        if type(Num) != int:
+            print('Incorrect value, please try again')
+
+    elif plr == 4:
+        print("It's ", names[4], " turn!")
+        print('Table has ', Count, ' matches, how much would you take?')
+        input(Num)
+        if type(Num) != int:
+            print('Incorrect value, please try again')
+
+    print(names[plr], "take ", Num, "Matches")
+    if Num >= 1 and Num <= 10 and Num <= Count:
+        Count = Count - int(Num)
+    else:
+        print('You must use 1-10 range, try again')
+    while Count > 0:
+        game(plr, NumOfPlayers, op)
+    return 0
 def __main__():
     Num = 0
-    Count = vMax
+    Count = MAX
+    names = ['Player 1', 'Player 2', 'Player 3', 'Player 4']
+    NumOfPlayers = int
+
+
     AIturn()
     while play:
         print('Number of opponents')
+        print('')
+        input(NumOfPlayers)
+        start(NumOfPlayers)
+        game(0, 0, 0)
 
     return 0
 
